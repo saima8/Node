38f9afa255e4fa4c27a6b8b912b76504e315e39e
Saima Khan <tithi888khan@gmail.com>
2021-02-16 01:28:55 +0600

    Added new notes with error handling

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,77 +1,56 @@
-// const fs = require('fs')
-// fs.writeFileSync('notes.txt','My name is Saima Khan. I am a Software Engineer.')
-
-// fs.appendFileSync('notes.txt','My initial salary should be 30k to 40k')
-// const name = require ('./utils.js')
-// const add = require ('./utils.js')
-// const sum = add(4,5)
-// // console.log(name)
-// console.log(sum);
-
-// const validator = require ('validator')
-
-
-
-// console.log(chalk.green.bold.underline.inverse('Success!'))
-// console.log(chalk.blue.bold.inverse('You can do it!'))
-
-// const lekha = notes();
-
-// console.log(lekha)
-// // console.log(validator.isEmail('example.com'))
-// // console.log(validator.isURL('https/google.com'))
-// console.log(process.argv[2])
-const yargs = require('yargs')
 const chalk = require('chalk')
-const notes = require('./notes')
-// customize yargs version
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
+// Customize yargs version
 yargs.version('1.1.0')
-yargs.command ({
+
+// Create add command
+yargs.command({
     command: 'add',
-    describe: 'add a new note',
+    describe: 'Add a new note',
     builder: {
         title: {
-            describe: 'New notes',
-            demandOption: false,
+            describe: 'Note title',
+            demandOption: true,
             type: 'string'
         },
         body: {
-            describe: 'Add body',
+            describe: 'Note body',
             demandOption: true,
             type: 'string'
         }
     },
     handler: function (argv) {
-        notes.addNotes(argv.title, argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
-// create remove command
-yargs.command ({
+
+// Create remove command
+yargs.command({
     command: 'remove',
-    describe: 'removing a note',
-    handler: function (){
-        console.log('a note is removed')
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
     }
 })
-//create list command 
+
+// Create list command
 yargs.command({
     command: 'list',
-    describe: 'listing the nodes',
-    handler: function (){
-        console.log('List the notes')
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
     }
 })
-//create read command
+
+// Create read command
 yargs.command({
-    command:'read',
-    describe:'Reading the lists',
-    
-    handler: function(){
-        console.log('Read the notes')
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
     }
 })
-// console.log(yargs.argv)
 
 yargs.parse()
-

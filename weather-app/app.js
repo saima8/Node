e32182ed2e679b19b9fd62fcf053ac7f02be5b04
Saima Khan <tithi888khan@gmail.com>
2021-02-17 01:40:04 +0600

    Added customized htp requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,5 +2,8 @@ const axios = require ('axios')
 
 axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d56894af0441d016d0dcc1106236c55a')
     .then((response) => {
-        console.log(response.data)
+        // console.log(response.data)
+        const temp = response.data.main.temp
+        const perc = response.data.clouds.all
+        console.log('it is currently '+temp +' out. '+ 'There is ' +perc + '% chance of rain')
     })

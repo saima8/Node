e9e14d599dfeefb4645118bc2bc89aca8747f10c
Saima Khan <tithi888khan@gmail.com>
2021-02-17 08:12:39 +0600

    Added 2 http requests from 2 API

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,3 +7,9 @@ axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d568
         const perc = response.data.clouds.all
         console.log('it is currently '+temp +' out. '+ 'There is ' +perc + '% chance of rain')
     })
+
+axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2FpbWFhMTgxIiwiYSI6ImNrbDhycjFkcTJpZ2Qyb29lOHN5ZTc3cW4ifQ.2MV1PumwU7OMNzpVpbZeBg&limit=1')
+    .then((response) => {
+        console.log('Longitute is ' +response.data.features[0].center[0])
+        console.log('Latitude is ' +response.data.features[0].center[1])
+    })
